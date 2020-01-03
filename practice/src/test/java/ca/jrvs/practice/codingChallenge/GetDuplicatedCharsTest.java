13a36cbddceaed8fd43b427282befecc5cd294d5
vassairm <vassairm@gmail.com>
2020-01-03 11:40:15 -0500

    Code Challenge - Get Duplicated Characters

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/GetDuplicatedCharsTest.java
@@ -0,0 +1,24 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GetDuplicatedCharsTest {
+
+  String test1 = "aaaaA";
+  String test2 = "";
+  String test3 = "The quick brown fox jumps over a lazy dog";
+  String test4 = "_101 Dalmations!!!_";
+
+  GetDuplicatedChars getDuplicatedChars = new GetDuplicatedChars();
+
+  @Test
+  public void getDuplicateCharsSet() {
+    assertArrayEquals(new char[]{'a'}, getDuplicatedChars.getDuplicateCharsSet(test1));
+    assertArrayEquals(new char[]{}, getDuplicatedChars.getDuplicateCharsSet(test2));
+    assertArrayEquals(new char[]{'o', 'u', 'e', 'r', 'a'}, getDuplicatedChars.getDuplicateCharsSet(test3));
+    assertArrayEquals(new char[]{'a'}, getDuplicatedChars.getDuplicateCharsSet(test4));
+
+  }
+}
