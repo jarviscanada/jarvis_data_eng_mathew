8a5ced38f266178c0d3d7e3a790c7204933db1eb
vassairm <vassairm@gmail.com>
2020-01-14 10:38:04 -0500

    Tests updated to comply with requested >75% line coverage

--- springboot/src/test/java/ca/jrvs/apps/trading/dao/MarketDataDaoIntTest.java
+++ springboot/src/test/java/ca/jrvs/apps/trading/dao/MarketDataDaoIntTest.java
@@ -2,10 +2,12 @@ package ca.jrvs.apps.trading.dao;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import ca.jrvs.apps.trading.model.domain.IexQuote;
 import ca.jrvs.apps.trading.config.MarketDataConfig;
+import ca.jrvs.apps.trading.model.domain.IexQuote;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -33,11 +35,62 @@ public class MarketDataDaoIntTest {
   }
 
   @Test
+  public void findById_NotExists() {
+    IexQuote quote = testDao.findById("Nonexistent").orElse(null);
+    assertNull(quote);
+  }
+
+  @Test
   public void findAllById() {
     List<String> symbols = Arrays.asList("aapl", "msft", "t", "tsla");
     Iterable<IexQuote> quotes = testDao.findAllById(symbols);
     for (IexQuote iex : quotes) {
       assertTrue(symbols.contains(iex.getSymbol().toLowerCase()));
     }
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void save() {
+    testDao.save(new IexQuote());
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void saveAll() {
+    testDao.saveAll(new ArrayList<>());
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void existsById() {
+    testDao.existsById("");
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void findAll() {
+    testDao.findAll();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void count() {
+    testDao.count();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void deleteById() {
+    testDao.deleteById("");
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void delete() {
+    testDao.delete(new IexQuote());
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void deleteAll() {
+    testDao.deleteAll();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDeleteAll() {
+    testDao.deleteAll(new ArrayList<>());
   }
 }
--- springboot/src/test/java/ca/jrvs/apps/trading/dao/QuoteDaoIntTest.java
+++ springboot/src/test/java/ca/jrvs/apps/trading/dao/QuoteDaoIntTest.java
@@ -15,11 +15,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestConfig.class)
+@ActiveProfiles("test")
 @Sql({"classpath:schema.sql"})
 public class QuoteDaoIntTest {
 
