224be3eeb96340b13d9af92c2ede4a25914c879e
vassairm <vassairm@gmail.com>
2020-01-22 12:00:28 -0500

    DashboardController code and tests. SecurityOrderDao updated to find orders by account ID

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
@@ -0,0 +1,66 @@
+package ca.jrvs.apps.trading.controller;
+
+import ca.jrvs.apps.trading.model.TraderAccountView;
+import ca.jrvs.apps.trading.model.domain.TraderPortfolioView;
+import ca.jrvs.apps.trading.service.DashboardService;
+import ca.jrvs.apps.trading.utils.ResponseExceptionUtils;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+  private DashboardService dashboardService;
+
+  @Autowired
+  public DashboardController(DashboardService dashboardService) {
+    this.dashboardService = dashboardService;
+  }
+
+  /**
+   * Get the Requested trader's profile and return it. Sample Usage: GET /dashboard/1543/profile
+   *
+   * @param traderId The ID of the trader to look up
+   * @return A TraderAccountView corresponding to the trader
+   */
+  @GetMapping("/{traderId}/profile")
+  @ResponseBody
+  @ResponseStatus(HttpStatus.OK)
+  @ApiOperation(value = "Get the Trader's profile", notes = "The Trader ID must exist",
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public TraderAccountView getTraderAccountView(@PathVariable int traderId) {
+    try {
+      return dashboardService.getTraderAccountView(traderId);
+    } catch (Exception ex) {
+      throw ResponseExceptionUtils.getResponseStatusException(ex);
+    }
+  }
+
+  /**
+   * Get a specific trader's portfolio and return it. Sample Usage: GET /dashboard/15432/portfolio
+   *
+   * @param traderId The ID of the trader to look up
+   * @return The TraderPortfolioView corresponding to the trader
+   */
+  @GetMapping("/{traderId}/portfolio")
+  @ResponseBody
+  @ResponseStatus(HttpStatus.OK)
+  @ApiOperation(value = "Get the trader's Portfolio", notes = "The trader must exist",
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public TraderPortfolioView getTraderPortfolioView(@PathVariable int traderId) {
+    try {
+      return dashboardService.getTraderPortfolioView(traderId);
+    } catch (Exception ex) {
+      throw ResponseExceptionUtils.getResponseStatusException(ex);
+    }
+  }
+}
---
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/DashboardService.java
@@ -0,0 +1,55 @@
+package ca.jrvs.apps.trading.service;
+
+import ca.jrvs.apps.trading.dao.AccountDao;
+import ca.jrvs.apps.trading.dao.SecurityOrderDao;
+import ca.jrvs.apps.trading.dao.TraderDao;
+import ca.jrvs.apps.trading.model.TraderAccountView;
+import ca.jrvs.apps.trading.model.domain.Account;
+import ca.jrvs.apps.trading.model.domain.Trader;
+import ca.jrvs.apps.trading.model.domain.TraderPortfolioView;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardService {
+
+  private AccountDao accountDao;
+  private TraderDao traderDao;
+  private SecurityOrderDao securityOrderDao;
+
+  @Autowired
+  public DashboardService(AccountDao accountDao, SecurityOrderDao securityOrderDao,
+      TraderDao traderDao) {
+    this.accountDao = accountDao;
+    this.securityOrderDao = securityOrderDao;
+    this.traderDao = traderDao;
+  }
+
+  /**
+   * Retrieve the trader's profile and account data.
+   *
+   * @param traderId The ID of the trader to look up
+   * @return The Trader's associated TraderAccountView
+   */
+  public TraderAccountView getTraderAccountView(int traderId) {
+    Trader trader = traderDao.findById(traderId).orElseThrow(EntityNotFoundException::new);
+    Account account = accountDao.findById(traderId).orElseThrow(EntityNotFoundException::new);
+    return new TraderAccountView(trader, account);
+  }
+
+  /**
+   * Retrieve a given Trader's Portfolio (The Traders' order history and currently available funds)
+   *
+   * @param traderId The ID of the trader to look up.
+   * @return The trader's portfolio
+   */
+  public TraderPortfolioView getTraderPortfolioView(int traderId) {
+    TraderPortfolioView portfolioView = new TraderPortfolioView();
+    portfolioView.setTraderId(traderId);
+    portfolioView.setOrders(securityOrderDao.findAllById(traderId));
+    portfolioView.setFunds(accountDao.findById(traderId).orElseThrow(EntityNotFoundException::new)
+        .getAmount());
+    return portfolioView;
+  }
+}
