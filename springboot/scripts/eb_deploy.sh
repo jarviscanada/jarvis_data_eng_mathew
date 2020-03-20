874691b8ed8901131d4653c7fa1f29892acf3380
vassairm <vassairm@gmail.com>
2020-03-20 15:33:49 -0400

    Springboot project setup for Jenkins

---
+++ springboot/scripts/eb_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+# arg count check
+if [ -z "$2" ]
+then
+  echo "Usage: eb_deploy.sh <app_name> <deploy_environment>"
+  exit 1
+fi
+
+app_name = $1
+deploy_env = $2
+
+# Init Beanstalk app and set environment
+eb init ${app_name} --platform java --region us-east-1
+eb use ${deploy_env}
+
+# Generate new deployment file
+rm -rf .elasticbeanstalk
+cat >> .elasticbeanstalk/config.yml << _EOF
+deploy:
+    artifact: springboot/target/trading-1.0-SNAPSHOT.jar
+_EOF
+
+# Deploy trading app
+eb deploy
