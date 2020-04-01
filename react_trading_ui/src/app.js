12d56b04aff6abf2fcb2b08a1f63dddc7b9df28c
vassairm <vassairm@gmail.com>
2020-04-01 13:13:28 -0400

    React project initial commit

---
+++ react_trading_ui/src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const fetch = require('node-fetch');
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+
+    render() {           
+        return(
+           <p>Placeholder</p> 
+        );
+    }
+}
